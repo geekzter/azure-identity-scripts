bfd3e55db621893dbf1f6c6a6548bd30b0f83b84
Eric van Wijk <eric@van-wijk.com>
2023-02-18 12:58:02 +0100

    Log json response

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -2,9 +2,11 @@ function Find-ApplicationByGUID (
     [parameter(Mandatory=$true)][guid]$Id
 ) {
     if ($FindApplication) {
-        az ad app show --id $Id 2>$null | ConvertFrom-Json | Set-Variable app
-        if ($app) {
+        az ad app show --id $Id 2>$null | Set-Variable jsonResponse
+        if ($jsonResponse) {
+            $jsonResponse | ConvertFrom-Json | Set-Variable app
             Write-Verbose "Found Application with Object ID '$Id' using:`naz ad app show --id ${Id}"
+            Write-JsonResponse -Json $jsonResponse
             return $app
         } else {
             return $null
@@ -16,28 +18,36 @@ function Find-ApplicationByName (
     [parameter(Mandatory=$true)][string]$Name
 ) {
     if ($FindApplication) {
-        az ad app list --display-name $Name --query "[0]" 2>$null | ConvertFrom-Json | Set-Variable app
-        if ($app) {
+        az ad app list --display-name $Name --query "[0]" 2>$null | Set-Variable jsonResponse
+        if ($jsonResponse) {
+            $jsonResponse | ConvertFrom-Json | Set-Variable app
             Write-Verbose "Found Application with name '$Name' using:`naz ad app list --display-name ${Name}"
+            Write-JsonResponse -Json $jsonResponse
             return $app
         } else {
             return $null
-        }            
+        }    
     }
 }
 
 function Find-ServicePrincipalByGUID (
     [parameter(Mandatory=$true)][guid]$Id
 ) {
-    az ad sp show --id $Id 2>$null | ConvertFrom-Json | Set-Variable sp
-    if ($sp) {
+    az ad sp show --id $Id 2>$null | Set-Variable jsonResponse
+    if ($jsonResponse) {
+        $jsonResponse | ConvertFrom-Json | Set-Variable sp
         Write-Verbose "Found Service Principal with Object ID '$Id' using:`naz ad sp show --id ${Id}"
+        Write-JsonResponse -Json $jsonResponse
+        return $sp
     } else {
-        az ad sp list --filter "appId eq '$Id'" --query "[0]" 2>$null | ConvertFrom-Json | Set-Variable sp
-        if ($sp) {
+        az ad sp list --filter "appId eq '$Id'" --query "[0]" 2>$null | Set-Variable jsonResponse
+        if ($jsonResponse) {
+            $jsonResponse | ConvertFrom-Json | Set-Variable sp
             Write-Verbose "Found Service Principal with Application ID '$Id' using:`naz ad sp list --filter `"appId eq '${Id}'`" --query `"[0]`""
+            Write-JsonResponse -Json $jsonResponse
+            return $sp
         }
-    }
+    }    
 
     return $sp
 }
@@ -59,17 +69,24 @@ function Find-ManagedIdentitiesByNameMicrosoftGraph (
                   -o json `
                   | Set-Variable jsonResponse
     if ($jsonResponse) {
-        if (Get-Command jq -ErrorAction SilentlyContinue) {
-            Write-Debug "resposne:"
-            $jsonResponse | jq -C | Write-Debug
-        }
         $jsonResponse | ConvertFrom-Json `
                       | Select-Object -Property name,appId,principalId,resourceId `
                       | Sort-Object -Property name `
                       | Set-Variable sps
         Write-Verbose "Found $(($sps | Measure-Object).Count) Managed Identities with name starting with '$StartsWith' using:"
         Write-Verbose "az ad sp list --filter `"startswith(displayName,'${StartsWith}') and servicePrincipalType eq 'ManagedIdentity'`" --query `"[${jmesPathQuery}].{name:displayName,appId:appId,principalId:id,resourceId:alternativeNames[1]}`" -o table"
+        Write-JsonResponse -Json $jsonResponse
         return $sps
+    }
+}
+
+function Write-JsonResponse (
+    [parameter(Mandatory=$true)]$Json
+) {
+    if (Get-Command jq -ErrorAction SilentlyContinue) {
+        $Json | jq -C | Write-Debug
+    } else {
+        Write-Debug $Json
     }
 }
 
@@ -97,16 +114,13 @@ function Find-ManagedIdentitiesByNameAzureResourceGraph (
                    -o json `
                    | Set-Variable jsonResponse
     if ($jsonResponse) {
-        if (Get-Command jq -ErrorAction SilentlyContinue) {
-            Write-Debug "resposne:"
-            $jsonResponse | jq -C | Write-Debug
-        }
         $jsonResponse | ConvertFrom-Json `
                       | Select-Object -Property name,appId,principalId,resourceId `
                       | Sort-Object -Property name `
                       | Set-Variable mis
         Write-Verbose "Found $(($mis | Measure-Object).Count) Managed Identities with name containing '$Search' using:"
         Write-Verbose "az graph query -q `"${resourceGraphQuery}`" -a --query `"data`""
+        Write-JsonResponse -Json $jsonResponse
         return $mis
     }
 }
@@ -141,31 +155,39 @@ function Find-ManagedIdentityByResourceID (
             --headers ConsistencyLevel=eventual `
             --query "value[0]" `
             -o json `
-            | ConvertFrom-Json `
-            | Set-Variable sp
-    if ($sp) {
+            | Set-Variable jsonResponse
+    if ($jsonResponse) {
+        $jsonResponse | ConvertFrom-Json `
+                      | Set-Variable sp
         $sp | Add-Member -NotePropertyName principalId -NotePropertyValue $sp.id
         Write-Verbose "Found Managed Identity with resourceId '$Id' using Microsoft Graph query:"
         Write-Verbose "az rest --method get --url `"${graphUrl}`" --headers ConsistencyLevel=eventual"
+        Write-JsonResponse -Json $jsonResponse
         return $sp
     }
 
     # Use ARM API for User-assigned Identity
     if ($IsUserIdentity) {
-        az identity show --ids $Id -o json 2>$null | ConvertFrom-Json | Set-Variable mi
-        if ($mi) {
+        az identity show --ids $Id -o json 2>$null | Set-Variable jsonResponse
+        if ($jsonResponse) {
+            $jsonResponse | ConvertFrom-Json `
+                          | Set-Variable mi
             Write-Verbose "Found User-assigned Identity with resourceId '$Id' using ARM API:"
             Write-Verbose "az identity show --ids $Id"
+            Write-JsonResponse -Json $jsonResponse
             return $mi
-        }    
+        }
     }
 
     # Use ARM API for System-assigned Identity
     if ($IsSysytemIdentity) {
-        az resource show --ids $Id --query "{id:id, principalId:identity.principalId, tenantId:tenantId}" -o json 2>$null | Set-Variable mi
-        if ($mi) {
-            Write-Verbose "Found System-managed Identity with resourceId '$Id' using ARM API:"
+        az resource show --ids $Id --query "{id:id, principalId:identity.principalId, tenantId:tenantId}" -o json 2>$null | Set-Variable jsonResponse
+        if ($jsonResponse) {
+            $jsonResponse | ConvertFrom-Json `
+                          | Set-Variable mi
+            Write-Verbose "Found System-assigned Identity with resourceId '$Id' using ARM API:"
             Write-Verbose "az resource show --ids $Id --query `"identity`" -o tsv"
+            Write-JsonResponse -Json $jsonResponse
             return $mi
         }
     }
@@ -174,28 +196,36 @@ function Find-ManagedIdentityByResourceID (
 function Find-ServicePrincipalByName (
     [parameter(Mandatory=$true)][string]$Name
 ) {
-    az ad sp show --id $Name 2>$null | ConvertFrom-Json | Set-Variable sp
-    if ($sp) {
+    az ad sp show --id $Name 2>$null | Set-Variable jsonResponse
+    if ($jsonResponse) {
+        $jsonResponse | ConvertFrom-Json | Set-Variable sp
         Write-Verbose "Found Service Principal with Application ID '$Name' using:"
         Write-Verbose "az ad sp show --id $Name"
+        Write-JsonResponse -Json $jsonResponse
         return $sp
-    } 
-    az ad sp list --filter "displayName eq '$Name'" --query "[0]" 2>$null | ConvertFrom-Json | Set-Variable sp
-    if ($sp) {
+    }
+    az ad sp list --filter "displayName eq '$Name'" --query "[0]" 2>$null | Set-Variable jsonResponse
+    if ($jsonResponse) {
+        $jsonResponse | ConvertFrom-Json | Set-Variable sp
         Write-Verbose "Found Service Principal with Display Name '$Name' using:"
         Write-Verbose "az ad sp list --filter `"displayName eq '$Name'`""
+        Write-JsonResponse -Json $jsonResponse
         return $sp
-    } 
-    az ad sp show --spn $Name 2>$null | ConvertFrom-Json | Set-Variable sp
-    if ($sp) {
+    }
+    az ad sp show --spn $Name 2>$null | Set-Variable jsonResponse
+    if ($jsonResponse) {
+        $jsonResponse | ConvertFrom-Json | Set-Variable sp
         Write-Verbose "Found Service Principal with Service Principal Name (SPN) '$Name' using:"
         Write-Verbose "az ad sp show --spn $Name"
+        Write-JsonResponse -Json $jsonResponse
         return $sp
-    } 
-    az ad sp list --show-mine --query "[?contains(servicePrincipalNames,'$Name')]|[0]" 2>$null | ConvertFrom-Json | Set-Variable sp
-    if ($sp) {
+    }
+    az ad sp list --show-mine --query "[?contains(servicePrincipalNames,'$Name')]|[0]" 2>$null | Set-Variable jsonResponse
+    if ($jsonResponse) {
+        $jsonResponse | ConvertFrom-Json | Set-Variable sp
         Write-Verbose "Found my Service Principal with '$Name' using Azure CLI JMESPath:"
         Write-Verbose "az ad sp list --show-mine --query `[?contains(servicePrincipalNames,'$Name')]|[0]`""
+        Write-JsonResponse -Json $jsonResponse
         return $sp
     }
 }
