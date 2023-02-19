b14757df1c1068ca053ea751f57205d1174af0d7
Eric van Wijk <eric@van-wijk.com>
2023-02-19 17:26:42 +0100

    Add # of passwordCredentials

--- scripts/functions.ps1
+++ scripts/functions.ps1
@@ -54,11 +54,11 @@ function Find-ApplicationsByName (
     [string]
     $StartsWith
 ) {
-    $graphUrl = "https://graph.microsoft.com/v1.0/applications?$count=true&`$filter=startswith(displayName,'${StartsWith}')&`$expand=federatedIdentityCredentials&`$select=id,appId,displayName,federatedIdentityCredentials"
-    Find-DirectoryObjectsByGraphUrl -GraphUrl $graphUrl -JmesPath "value[].{name:displayName,appId:appId,id:id,federationSubjects:join(',',federatedIdentityCredentials[].subject)}" | Set-Variable apps
+    $graphUrl = "https://graph.microsoft.com/v1.0/applications?$count=true&`$filter=startswith(displayName,'${StartsWith}')&`$expand=federatedIdentityCredentials&`$select=id,appId,displayName,federatedIdentityCredentials,passwordCredentials"
+    Find-DirectoryObjectsByGraphUrl -GraphUrl $graphUrl -JmesPath "value[].{name:displayName,appId:appId,id:id,federationSubjects:join(',',federatedIdentityCredentials[].subject),passwordCredentials:length(passwordCredentials[])}" | Set-Variable apps
 
     if ($apps) {
-        $apps | Select-Object -Property name,appId,id,federationSubjects `
+        $apps | Select-Object -Property name,appId,id,federationSubjects,passwordCredentials `
               | Sort-Object -Property name `
               | Set-Variable apps
         Write-Verbose "Found Managed Identity with resourceId '$Id' using Microsoft Graph query:"
