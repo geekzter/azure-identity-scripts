a310f458d27c345d9ed0682abbb15f6d5bdb6d61
Eric van Wijk <eric@van-wijk.com>
2023-07-24 18:22:22 +0200

    Use projectCollections API

--- terraform/azure-devops/create-service-connection/azdo_organization.tf
+++ terraform/azure-devops/create-service-connection/azdo_organization.tf
@@ -1,6 +1,7 @@
 locals {
   azdo_member_id               = jsondecode(data.http.azdo_member.response_body).id
-  azdo_organization_id         = var.azdo_organization_id != null && var.azdo_organization_id != "" ? var.azdo_organization_id : [for a in jsondecode(data.http.azdo_organizations.response_body).value : a.accountId if a.accountName == local.azdo_organization_name][0]
+  # azdo_organization_id         = var.azdo_organization_id != null && var.azdo_organization_id != "" ? var.azdo_organization_id : [for a in jsondecode(data.http.azdo_organizations.response_body).value : a.accountId if a.accountName == local.azdo_organization_name][0]
+  azdo_organization_id         = var.azdo_organization_id != null && var.azdo_organization_id != "" ? var.azdo_organization_id : jsondecode(data.http.azdo_organization.response_body).value[0].id
 }
 
 data http azdo_member {
@@ -34,6 +35,26 @@ data http azdo_organizations {
     postcondition {
       condition                = length(jsondecode(self.response_body).value) > 0
       error_message            = "No organizations found for member ${local.azdo_member_id}"
+    }
+  }
+}
+
+data http azdo_organization {
+  url                          = "${local.azdo_organization_url}/_apis/projectCollections?api-version=7.1-preview.1"
+
+  request_headers = {
+    Accept                     = "application/json"
+    Authorization              = "Bearer ${data.external.azdo_token.result.accessToken}"
+  }
+
+  lifecycle {
+    postcondition {
+      condition                = tonumber(self.status_code) < 300
+      error_message            = "Could not retrieve member information"
+    }
+    postcondition {
+      condition                = length(jsondecode(self.response_body).value) == 1
+      error_message            = "${length(jsondecode(self.response_body).value)} project collections found, 1 expected"
     }
   }
 }
