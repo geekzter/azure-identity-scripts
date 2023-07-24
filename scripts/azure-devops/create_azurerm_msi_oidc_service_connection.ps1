bd6f553d33513b456487b0186203cd311af285f4
Eric van Wijk <eric@van-wijk.com>
2023-07-24 18:43:49 +0200

    Get org id

--- scripts/azure-devops/create_azurerm_msi_oidc_service_connection.ps1
+++ scripts/azure-devops/create_azurerm_msi_oidc_service_connection.ps1
@@ -209,12 +209,24 @@ az identity create -n $IdentityName `
                    | Set-Variable identity
 Write-Verbose "Created Managed Identity $($identity.id)"
 
+# Look up organizationId
+Write-Verbose "Looking up Azure DevOps organization id (required for issuer)..."
+az rest --method get `
+        --uri "${OrganizationUrl}/_apis/projectCollections?api-version=7.1-preview.1" `
+        --resource 499b84ac-1321-427f-aa17-267ca6975798 `
+        -o tsv `
+        --query "value[0].id" `
+        | Set-Variable organizationId
+
+$issuerUrl = "https://app.vstoken.visualstudio.com"
+# $issuerUrl = "https://vstoken.dev.azure.com/${organizationId}"
+
 $federatedSubject = "sc://${organizationName}/${Project}/${ServiceConnectionName}"
 Write-Verbose "Configuring Managed Identity '${IdentityName}' with federated subject '${federatedSubject}'..."
 az identity federated-credential create --name $IdentityName `
                                         --identity-name $IdentityName  `
                                         --resource-group $IdentityResourceGroupName `
-                                        --issuer https://app.vstoken.visualstudio.com `
+                                        --issuer $issuerUrl `
                                         --subject $federatedSubject `
                                         --subscription $IdentitySubscriptionId `
                                         -o json `
