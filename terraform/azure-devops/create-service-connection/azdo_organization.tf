30961676255b0c02f9dab56ec2b390d6d35bdcd2
Eric van Wijk <eric@van-wijk.com>
2023-07-26 21:08:54 +0200

    Use profile & accounts API

--- terraform/azure-devops/create-service-connection/azdo_organization.tf
+++ terraform/azure-devops/create-service-connection/azdo_organization.tf
@@ -1,8 +1,11 @@
 locals {
-  azdo_organization_id         = var.azdo_organization_id != null && var.azdo_organization_id != "" ? var.azdo_organization_id : jsondecode(data.http.azdo_organization.response_body).value[0].id
+  azdo_api_version             = "7.1-preview.1"
+  azdo_member_id               = jsondecode(data.http.azdo_member.response_body).id
+  azdo_organization_id         = var.azdo_organization_id != null && var.azdo_organization_id != "" ? var.azdo_organization_id : [for a in jsondecode(data.http.azdo_organizations.response_body).value : a.accountId if a.accountName == local.azdo_organization_name][0]
 }
-data http azdo_organization {
-  url                          = "${local.azdo_organization_url}/_apis/projectCollections?api-version=7.1-preview.1"
+
+data http azdo_member {
+  url                          = "https://app.vssps.visualstudio.com/_apis/profile/profiles/me?api-version=${local.azdo_api_version}"
 
   request_headers = {
     Accept                     = "application/json"
@@ -15,8 +18,27 @@ data http azdo_organization {
       error_message            = "Could not retrieve member information"
     }
     postcondition {
-      condition                = length(jsondecode(self.response_body).value) == 1
-      error_message            = "${length(jsondecode(self.response_body).value)} project collections found, 1 expected"
+      condition                = can(jsondecode(self.response_body).id)
+      error_message            = "No member information or not in expected format"
+    }
+  }
+}
+
+data http azdo_organizations {
+  url                          = "https://app.vssps.visualstudio.com/_apis/accounts?api-version=${local.azdo_api_version}&memberId=${local.azdo_member_id}"
+  request_headers = {
+    Accept                     = "application/json"
+    Authorization              = "Bearer ${data.external.azdo_token.result.accessToken}"
+  }
+
+  lifecycle {
+    postcondition {
+      condition                = tonumber(self.status_code) < 300
+      error_message            = "Could not retrieve account information"
+    }
+    postcondition {
+      condition                = length(jsondecode(self.response_body).value) > 0
+      error_message            = "No organizations found for member ${local.azdo_member_id}"
     }
   }
 }
