b5280b6d49b15442f641fd61b13360c3a1284276
Eric van Wijk <eric@van-wijk.com>
2024-03-25 15:01:47 +0100

    Improve service connection identity rename (#49)
    * Process multiple projects
    
    * Process Docker service connections
    
    * List service connection type

--- scripts/azure-devops/rename_service_connection_applications.ps1
+++ scripts/azure-devops/rename_service_connection_applications.ps1
@@ -14,8 +14,8 @@ param (
 
     [parameter(Mandatory=$false,HelpMessage="Name of the Azure DevOps Project")]
     [ValidateNotNullOrEmpty()]
-    [string]
-    $Project=$env:SYSTEM_TEAMPROJECT,
+    [string[]]
+    $Project=@($env:SYSTEM_TEAMPROJECT),
 
     [parameter(Mandatory=$false)]
     [switch]
@@ -38,69 +38,87 @@ Write-Verbose "Logging into Azure..."
 Login-Az -Tenant ([ref]$TenantId)
 
 
-# Get owned service connections using AAD tenant
+# Get org & user information
 $organizationName=($OrganizationUrl -split '/' | Select-Object -Index 3)
 $OrganizationUrl -replace "/*$", "" | Set-Variable -Name OrganizationUrl
 $userName=(az account show --query user.name -o tsv)
-az devops service-endpoint list --project $Project `
-                                --organization $OrganizationUrl `
-                                --query "[?authorization.parameters.serviceprincipalid!=null && authorization.parameters.tenantid=='${TenantId}' && createdBy.uniqueName=='${userName}']" `
-                                -o json `
-                                | ConvertFrom-Json `
-                                | Set-Variable -Name serviceConnections
 
-$serviceConnections | Format-Table -AutoSize -Property Name | Out-String | Write-Debug
+foreach ($azdoProject in $Project) {
+    # Get owned service connections using AAD tenant
 
-# Iterate through service connections
-"Processing service connections referencing an AAD application in {0}/{1}/_settings/adminservices created by {2}..." -f $OrganizationUrl, [uri]::EscapeDataString($Project), $userName | Write-Host
-foreach ($serviceConnection in $serviceConnections) {
-    # Get application
-    Write-Verbose "Getting application '$($serviceConnection.authorization.parameters.serviceprincipalid)' for service connection '$($serviceConnection.name)'..."
-    $application = $null
-    az ad app list --app-id $serviceConnection.authorization.parameters.serviceprincipalid `
-                   --query "[0]" `
-                   -o json `
-                   | ConvertFrom-Json `
-                   | Set-Variable -Name application
-    if (!$application) {
-        Write-Host "Application for service connection '$($serviceConnection.name)' not found, the service connection is using a Managed Identity or may be orphaned"
-        continue
+    az devops service-endpoint list --project $azdoProject `
+                                    --organization $OrganizationUrl `
+                                    --query "[?(authorization.parameters.serviceprincipalid!=null || authorization.parameters.servicePrincipalId!=null) && (authorization.parameters.tenantid=='${TenantId}' || authorization.parameters.tenantId=='${TenantId}') && createdBy.uniqueName=='${userName}']" `
+                                    -o json `
+                                    | ConvertFrom-Json `
+                                    | Set-Variable -Name serviceConnections
+
+    $serviceConnections | Format-Table -AutoSize -Property Name | Out-String | Write-Debug
+
+    # Iterate through service connections
+    "Processing service connections referencing an AAD application in {0}/{1}/_settings/adminservices created by {2}..." -f $OrganizationUrl, [uri]::EscapeDataString($azdoProject), $userName | Write-Host
+    foreach ($serviceConnection in $serviceConnections) {
+        # Do not rename service connections shared from another project
+        "{0}/{1}/_settings/adminservices?resourceId={2}" -f $OrganizationUrl, $serviceConnection.serviceEndpointProjectReferences[0].projectReference.name, $serviceConnection.id | Set-Variable originalServiceEndpointUrl
+        if ($serviceConnection.isShared) {
+            if ($serviceConnection.serviceEndpointProjectReferences[0].projectReference.name -ine $azdoProject) {
+                Write-Host "Skipping service connection '$($PSStyle.Bold)$($serviceConnection.name)$($PSStyle.BoldOff)' because it is shared from project $($PSStyle.Bold)$($serviceConnection.serviceEndpointProjectReferences[0].projectReference.name)$($PSStyle.BoldOff) : ${originalServiceEndpointUrl}"
+                continue
+            }
+            Write-Host "Service connection '$($serviceConnection.name)' is shared with the following projects:"
+            $serviceConnection.serviceEndpointProjectReferences | ForEach-Object {
+                "Service connection $($PSStyle.Bold){2}$($PSStyle.BoldOff) in project $($PSStyle.Bold){1}$($PSStyle.BoldOff) ({0}/{1}/_settings/adminservices?resourceId={3})" -f $OrganizationUrl, $_.projectReference.name, $_.name, $serviceConnection.id | Write-Host
+            }
+        }
+
+        # Get application
+        Write-Verbose "Getting application '$($serviceConnection.authorization.parameters.serviceprincipalid)' for service connection '$($serviceConnection.name)'..."
+        $application = $null
+        az ad app list --app-id $serviceConnection.authorization.parameters.serviceprincipalid `
+                    --query "[0]" `
+                    -o json `
+                    | ConvertFrom-Json `
+                    | Set-Variable -Name application
+        if (!$application) {
+            Write-Host "Application for service connection '$($PSStyle.Bold)$($serviceConnection.name)$($PSStyle.BoldOff)' not found, the service connection is using a Managed Identity or may be orphaned"
+            continue
+        }
+        $application | Format-List | Out-String | Write-Debug
+        Write-Verbose "Application displayName: $($application.displayName)"
+
+        # Determine default and new application names
+        "{0}-{1}-{2}" -f $organizationName, $azdoProject, $serviceConnection.data.subscriptionId `
+                    | Set-Variable -Name defaultApplicationName
+        Write-Verbose "Default application name: ${defaultApplicationName}"
+        if ($Reset) {
+            $newApplicationName = $defaultApplicationName
+        } else {
+            "{0}-{1}-{2}" -f $organizationName, $azdoProject, $serviceConnection.name `
+                        | Set-Variable -Name newApplicationName
+        }
+        Write-Verbose "New application name: ${newApplicationName}"
+
+        # Determine whether app has been renamed
+        $serviceConnection | Add-Member "oldApplicationName" $application.displayName
+        if ($application.displayName -eq $newApplicationName) {
+            Write-Host "Application for service connection '$($PSStyle.Bold)$($serviceConnection.name)$($PSStyle.BoldOff)' has already been renamed to '$($PSStyle.Bold)${newApplicationName}$($PSStyle.BoldOff)'"
+            continue
+        }
+
+        # Rename app
+        $serviceConnection | Add-Member "newApplicationName" $newApplicationName
+        Write-Host "Renaming application $($PSStyle.Bold)$($application.displayName)$($PSStyle.BoldOff) to '$($PSStyle.Bold)${newApplicationName}$($PSStyle.BoldOff)'..." -Nonewline
+        if ($WhatIf) {
+            Write-Host " skipped (WhatIf specified)"
+            continue
+        } else {
+            Write-Host ""
+        }
+        az ad app update --id $application.appId `
+                        --display-name $newApplicationName
     }
-    $application | Format-List | Out-String | Write-Debug
-    Write-Verbose "Application displayName: $($application.displayName)"
 
-    # Determine default and new application names
-    "{0}-{1}-{2}" -f $organizationName, $Project, $serviceConnection.data.subscriptionId `
-                  | Set-Variable -Name defaultApplicationName
-                  Write-Verbose "Default application name: ${defaultApplicationName}"
-    if ($Reset) {
-        $newApplicationName = $defaultApplicationName
-    } else {
-        "{0}-{1}-{2}" -f $organizationName, $Project, $serviceConnection.name `
-                      | Set-Variable -Name newApplicationName
-    }
-    Write-Verbose "New application name: ${newApplicationName}"
-
-    # Determine whether app has been renamed
-    $serviceConnection | Add-Member "oldApplicationName" $application.displayName
-    if ($application.displayName -eq $newApplicationName) {
-        Write-Host "Application for service connection '$($serviceConnection.name)' has already been renamed to '${newApplicationName}'"
-        continue
-    }
-
-    # Rename app
-    $serviceConnection | Add-Member "newApplicationName" $newApplicationName
-    Write-Host "Renaming application $($application.displayName) to '${newApplicationName}'..." -Nonewline
-    if ($WhatIf) {
-        Write-Host " skipped (WhatIf specified)"
-        continue
-    } else {
-        Write-Host ""
-    }
-    az ad app update --id $application.appId `
-                     --display-name $newApplicationName
+    # List processed service connection identities
+    "`nService connections processed referencing an AAD application in {0}/{1}/_settings/adminservices created by {2}:" -f $OrganizationUrl, [uri]::EscapeDataString($azdoProject), $userName | Write-Host
+    $serviceConnections | Format-Table -AutoSize -Property Name, @{Name="clientId";Expression={$_.authorization.parameters.serviceprincipalid}}, oldApplicationName, newApplicationName, type
 }
-
-# List processed service connection identities
-"`nService connections processed referencing an AAD application in {0}/{1}/_settings/adminservices created by {2}:" -f $OrganizationUrl, [uri]::EscapeDataString($Project), $userName | Write-Host
-$serviceConnections | Format-Table -AutoSize -Property Name, @{Name="clientId";Expression={$_.authorization.parameters.serviceprincipalid}}, oldApplicationName, newApplicationName
