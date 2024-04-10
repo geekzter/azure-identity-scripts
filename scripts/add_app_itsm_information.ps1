97dea2bfd211f19b2dc185fe52cda7c4f4bbeaa2
Eric van Wijk <eric@van-wijk.com>
2024-04-10 11:20:00 +0200

    Add co-owner

--- scripts/add_app_itsm_information.ps1
+++ scripts/add_app_itsm_information.ps1
@@ -2,13 +2,25 @@
 #Requires -Version 7
 <#
 .SYNOPSIS 
-    Confugures Service Management Reference on Entra ID application
+    Configures Service Management Reference on Entra ID application
+.EXAMPLE
+    ./add_app_itsm_information.ps1 -AppId 00000000-0000-0000-00000000000000000 -ServiceManagementReferenceId 00000000-0000-0000-00000000000000000
+.EXAMPLE
+    ./add_app_itsm_information.ps1 -ServiceManagementReferenceId 00000000-0000-0000-00000000000000000 -CoOwner johndoe@fabrikam.com -Force
 #>
 param ( 
     [parameter(Mandatory=$false)]
     [guid]
     $AppId,
     
+    [parameter(Mandatory=$false)]
+    [string]
+    $CoOwner,
+
+    [parameter(Mandatory=$false)]
+    [switch]
+    $Force=$false,
+
     [parameter(Mandatory=$true)]
     [guid]
     $ServiceManagementReferenceId,
@@ -33,23 +45,38 @@ if ($AppId) {
     $apps = @($app)
     $app | Format-List | Out-String | Write-Verbose
 } else {
+    $query = $Force ? "[]" : "[?serviceManagementReference!='${ServiceManagementReferenceId}']"
     az ad app list --show-mine `
                    -o json `
-                   --query "[?serviceManagementReference!='${ServiceManagementReferenceId}']" `
+                   --query "${query}" `
                    | ConvertFrom-Json `
                    | Set-Variable -Name Apps
     $apps | Format-Table -Property id,displayName -AutoSize | Out-String | Write-Verbose
 }
 
+if ($CoOwner) {
+    az ad user show --id $CoOwner `
+                    -o tsv `
+                    --query id `
+                    | Set-Variable -Name coOwnerId
+}
+
 foreach ($app in $apps) {
-    if ($app.serviceManagementReference) {
-        Write-Warning "Service Management Reference already on app '$($app.displayName)' with id '$($app.id)' already set to '$($app.serviceManagementReference)'. Skipping..."
+    if (!$Force -and $app.serviceManagementReference) {
+        Write-Warning "Service Management Reference already on app '$($app.displayName)' with id '$($app.id)' already set to '$($app.serviceManagementReference)'. Skipping Service Management Reference update."
         continue
     }
     Write-Verbose "Adding Service Management Reference to app '$($app.displayName)' with id '$($app.id)'..."
     az ad app update --id $app.id `
                      --set serviceManagementReference=$ServiceManagementReferenceId `
                      -o none
+
+    if ($coOwnerId) {
+        Write-Verbose "Adding co-owner '$CoOwner' to app '$($app.displayName)' with id '$($app.id)'..."
+        az ad app owner add --id $app.id `
+                            --owner-object-id $coOwnerId `
+                            -o none
+    }
 }
 
 if ($AppId) {
