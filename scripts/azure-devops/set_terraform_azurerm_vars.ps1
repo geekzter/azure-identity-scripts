06e1f2727b6ddbf56e5416a417b53398155e9602
Eric van Wijk <eric@van-wijk.com>
2024-04-27 09:54:39 +0200

    7.1-preview.2

--- scripts/azure-devops/set_terraform_azurerm_vars.ps1
+++ scripts/azure-devops/set_terraform_azurerm_vars.ps1
@@ -36,7 +36,7 @@ function Get-OidcRequestUrl()
     if (!$env:AZURESUBSCRIPTION_SERVICE_CONNECTION_ID) {
         throw "Unable to determine service connection ID"
     }
-    $apiVersion = "7.1-preview.4"
+    $apiVersion = "7.1-preview.2"
     $oidcRequestUrl = "${env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}${env:SYSTEM_TEAMPROJECTID}/_apis/distributedtask/hubs/build/plans/${env:SYSTEM_PLANID}/jobs/${env:SYSTEM_JOBID}/oidctoken?api-version=${apiVersion}&serviceConnectionId=${env:AZURESUBSCRIPTION_SERVICE_CONNECTION_ID}"
     # $oidcRequestUrl = "${env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI}${env:SYSTEM_TEAMPROJECTID}/_apis/distributedtask/hubs/build/plans/${env:SYSTEM_PLANID}/jobs/${env:SYSTEM_JOBID}/oidctoken?serviceConnectionId=${env:AZURESUBSCRIPTION_SERVICE_CONNECTION_ID}"
     Write-Debug "OIDC Request URL: ${oidcRequestUrl}"
