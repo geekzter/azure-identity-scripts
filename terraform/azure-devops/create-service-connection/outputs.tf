abb50ae0fe5c3e992e7a72244fe14d0b795b3261
Eric van Wijk <eric@van-wijk.com>
2024-04-27 14:50:48 +0200

    Add descriptions to inputs & outputs

--- terraform/azure-devops/create-service-connection/outputs.tf
+++ terraform/azure-devops/create-service-connection/outputs.tf
@@ -1,63 +1,82 @@
 output azdo_project_id {
+  description = "The Azure DevOps project id the service connection was created in"
   value       = module.service_connection.project_id
 }
 output azdo_service_connection_id {
+  description = "The Azure DevOps service connection id"
   value       = module.service_connection.service_connection_id
 }
 output azdo_service_connection_name {
+  description = "The Azure DevOps service connection name"
   value       = local.azdo_service_connection_name
 }
 output azdo_service_connection_url {
+  description = "The Azure DevOps service connection portal URL"
   value       = module.service_connection.service_connection_url
 }
 output azure_resource_group_name {
+  description = "The name of the resource group the service connection was granted access to"
   value       = try(split("/", local.azure_scope)[4],null)
 }
 output azure_scope {
+  description = "The Azure scope the service connection was granted access to"
   value       = local.azure_scope
 }
 output azure_scope_url {
+  description = "The Azure scope portal URL the service connection was granted access to"
   value       = module.azure_access.resource_url
 }
 output azure_subscription_id {
+  description = "The Azure subscription id the service connection was granted access to"
   value       = local.target_subscription_id
 }
 output azure_subscription_name {
+  description = "The Azure subscription name the service connection was granted access to"
   value       = module.azure_access.subscription_name
 }
 
 output identity_application_id {
+  description = "The app/client id of the service connection's identity"
   value       = local.application_id
 }
 output identity_application_name {
+  description = "The name of the service connection's identity"
   value       = var.azdo_creates_identity || var.create_managed_identity ? null : module.entra_app.0.application_name
 }
 output identity_federation_subject {
+  description = "The federation subject"
   value       = module.service_connection.service_connection_oidc_subject
 }
 output identity_issuer {
+  description = "The federation issuer"
   value       = module.service_connection.service_connection_oidc_issuer
 }
 output identity_object_id {
+  description = "The object id of the service connection's identity"
   value       = var.azdo_creates_identity || var.create_managed_identity ? null : module.entra_app.0.object_id
 }
 output identity_principal_id {
+  description = "The service principal id of the service connection's identity"
   value       = local.principal_id
 }
 output identity_principal_name {
+  description = "The service principal name of the service connection's identity"
   value       = local.principal_name
 }
 output identity_principal_url {
+  description = "The service principal portal url of the service connection's identity"
   value       = var.azdo_creates_identity ? null : (var.create_managed_identity ? module.managed_identity.0.principal_url : module.entra_app.0.principal_url)
 }
 output identity_secret {
+  description = "The secret of the service connection's identity"
   sensitive   = true
   value       = var.azdo_creates_identity || var.create_managed_identity ? null : module.entra_app.0.secret
 }
 output identity_secret_end_date {
+  description = "The secret expiration date of the service connection's identity"
   value       = var.azdo_creates_identity || var.create_managed_identity ? null : module.entra_app.0.secret_end_date
 }
 output identity_url {
+  description = "The portal url of the service connection's identity"
   value       = var.azdo_creates_identity ? null : (var.create_managed_identity ? module.managed_identity.0.identity_url : module.entra_app.0.application_url)
 }
-
--- terraform/azure-devops/create-service-connection/variables.tf
+++ terraform/azure-devops/create-service-connection/variables.tf
@@ -1,31 +1,36 @@
 variable azdo_creates_identity {
-  description                  = "Let Azure DevOps create App Registration"
+  description                  = "Let Azure DevOps create identity for service connection"
   default                      = false
   type                         = bool
 }
-variable azdo_organization_id {
-  default                      = null
-  nullable                     = true
-}
+
 variable azdo_organization_url {
+  description                  = "The Azure DevOps organization URL (e.g. https://dev.azure.com/contoso)"
   nullable                     = false
+  type                         = string
 }
 variable azdo_project_name {
+  description                  = "The Azure DevOps project name to create the service connection in"
   nullable                     = false
+  type                         = string
 }
 
 variable azure_scope {
-  description                  = "The Azure scope to assign access to"
   default                      = null
+  description                  = "The Azure scope to assign access to"
+  type                         = string
 }
 
 variable azure_role {
   default                      = "Contributor"
+  description                  = "The Azure RBAC role to assign to the service connection's identity"
   nullable                     = false
+  type                         = string
 }
 
 variable azure_role_assignments {
   default                      = []
+  description                  = "Additional role assignments to create for the service connection's identity"
   nullable                     = true
   type                         = set(object({scope=string, role=string}))
 }
@@ -44,7 +49,7 @@ variable create_managed_identity {
 
 variable entra_owner_object_ids {
   default                      = null
-  description                  = "The object ids of the users that will be owners of the Entra ID app registration"
+  description                  = "Object ids of the users that will be co-owners of the Entra ID app registration"
   type                         = list(string)
 }
 
@@ -55,25 +60,30 @@ variable entra_secret_expiration_days {
 }
 
 variable entra_service_management_reference {
-  description                  = "IT Service Management Reference"
   default                      = null
+  description                  = "IT Service Management Reference to add to the App Registration"
+  type                         = string
 }
 
 variable managed_identity_resource_group_id {
   default                      = null
   description                  = "The resource group to create the Managed Identity in"
+  type                         = string
 }
 
 variable resource_prefix {
-  description                  = "The prefix to put at the end of resource names created"
+  description                  = "The prefix to put in front of resource names created"
   default                      = "demo"
   nullable                     = false
+  type                         = string
 }
 variable resource_suffix {
-  description                  = "The suffix to put at the of resource names created"
+  description                  = "The suffix to append to resource names created"
   default                      = "" # Empty string triggers a random suffix
+  type                         = string
 }
 variable run_id {
-  description                  = "The ID that identifies the pipeline / workflow that invoked Terraform"
+  description                  = "The ID that identifies the pipeline / workflow that invoked Terraform (used in CI/CD)"
   default                      = null
+  type                         = number
 }
